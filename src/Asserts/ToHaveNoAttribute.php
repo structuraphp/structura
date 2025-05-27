88e3c22f5cc42ec1990cba50b8192c6b6c30463f
mathieu-noel <noel-mathieu@hotmail.fr>
2025-05-27 21:45:38 +0200

    feat(assert): add to have no attribute and to have only attribute

---
+++ src/Asserts/ToHaveNoAttribute.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StructuraPhp\Structura\Asserts;
+
+use StructuraPhp\Structura\Contracts\ExprInterface;
+use StructuraPhp\Structura\ValueObjects\ClassDescription;
+use StructuraPhp\Structura\ValueObjects\ViolationValueObject;
+
+final readonly class ToHaveNoAttribute implements ExprInterface
+{
+    public function __construct(
+        private string $message = '',
+    ) {}
+
+    public function __toString(): string
+    {
+        return 'to have no attribute';
+    }
+
+    public function assert(ClassDescription $class): bool
+    {
+        return $class->attrGroups === [];
+    }
+
+    public function getViolation(ClassDescription $class): ViolationValueObject
+    {
+        return new ViolationValueObject(
+            \sprintf(
+                'Resource <promote>%s</promote> must not have attribute',
+                $class->isAnonymous()
+                    ? 'Anonymous'
+                    : $class->namespace,
+            ),
+            $this::class,
+            $class->lines,
+            $class->getFileBasename(),
+            $this->message,
+        );
+    }
+}
