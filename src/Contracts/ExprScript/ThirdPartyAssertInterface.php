07ccb6a1409ecab95df301b6d3e37caece035552
mathieu-noel <noel-mathieu@hotmail.fr>
2026-04-23 21:49:33 +0200

    fix(thrid-party): add use include in interface

--- src/Contracts/ExprScript/ThirdPartyAssertInterface.php
+++ src/Contracts/ExprScript/ThirdPartyAssertInterface.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace StructuraPhp\Structura\Contracts\ExprScript;
 
+use StructuraPhp\Structura\Enums\IncludeType;
+
 interface ThirdPartyAssertInterface
 {
     public function toUseStrictTypes(string $message = ''): self;
 
     public function toUseDeclare(string $key, string $value, string $message = ''): self;
+
+    public function toUseInclude(IncludeType $includeType, string $message = ''): self;
+
+    public function toNotUseInclude(string $message = ''): self;
 
     public function toHaveAnonymousClass(string $message = ''): self;
 
