15ee7fb458ea76e5cda88455999024ce4d319795
Clément FERRIER <clement.ferrier@helloce.fr>
2026-06-18 11:25:42 +0200

    fix(colors): compatibility with Ansi4 terminal

--- src/Console/Enums/StyleCustom.php
+++ src/Console/Enums/StyleCustom.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace StructuraPhp\Structura\Console\Enums;
 
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Output\AnsiColorMode;
+use Symfony\Component\Console\Terminal;
 
 enum StyleCustom: string
 {
@@ -20,13 +22,22 @@ enum StyleCustom: string
 
     public function getOutputFormatterStyle(): OutputFormatterStyle
     {
+        $teal = match (Terminal::getColorMode()) {
+            AnsiColorMode::Ansi4 => 'cyan',
+            default => '#066',
+        };
+        $orange =  match (Terminal::getColorMode()) {
+            AnsiColorMode::Ansi4 => 'bright-yellow',
+            default => '#fa0',
+        };
+
         return match ($this) {
             self::Fire => new OutputFormatterStyle('red', null, ['bold']),
             self::Green => new OutputFormatterStyle('green', null, ['bold']),
-            self::Notice => new OutputFormatterStyle(null, '#fa0', ['bold']),
-            self::Orange => new OutputFormatterStyle('#fa0', null, ['bold']),
+            self::Notice => new OutputFormatterStyle(null, $orange, ['bold']),
+            self::Orange => new OutputFormatterStyle($orange, null, ['bold']),
             self::Pass => new OutputFormatterStyle(null, 'green', ['bold']),
-            self::Promote => new OutputFormatterStyle('#066', null, ['bold']),
+            self::Promote => new OutputFormatterStyle($teal, null, ['bold']),
             self::Violation => new OutputFormatterStyle(null, 'red', ['bold']),
             self::Warning => new OutputFormatterStyle(null, 'yellow', ['bold']),
             self::Yellow => new OutputFormatterStyle('yellow', null, ['bold']),
